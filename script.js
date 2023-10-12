e5bb72df8daf2f72230c62b514d8d72b0e93833f
RavitejaMulaparthi <137281804+RavitejaMulaparthi@users.noreply.github.com>
2023-10-12 16:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(age) {
+    this._age = age;
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Example usage:
+const person1 = new Person("John", 30);
+console.log(`Name: ${person1.name}`);
+console.log(`Age: ${person1.age}`);
+person1.age = 35; // You can set the age using the setter
+console.log(`Updated Age: ${person1.age}`);
+
+const student1 = new Student("Alice", 20);
+console.log(`Name: ${student1.name}`);
+student1.study(); // This will call the study method specific to the Student class
+
+const teacher1 = new Teacher("Mr. Smith", 40);
+console.log(`Name: ${teacher1.name}`);
+teacher1.teach(); // This will call the teach method specific to the Teacher class
