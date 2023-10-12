7e1630797cc69a1a177637c3d407cd157c6581bf
RavitejaMulaparthi <137281804+RavitejaMulaparthi@users.noreply.github.com>
2023-10-12 16:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,18 +24,3 @@ class Teacher extends Person {
     console.log(`${this.name} is teaching`);
   }
 }
-
-// Example usage:
-const person1 = new Person("John", 30);
-console.log(`Name: ${person1.name}`);
-console.log(`Age: ${person1.age}`);
-person1.age = 35; // You can set the age using the setter
-console.log(`Updated Age: ${person1.age}`);
-
-const student1 = new Student("Alice", 20);
-console.log(`Name: ${student1.name}`);
-student1.study(); // This will call the study method specific to the Student class
-
-const teacher1 = new Teacher("Mr. Smith", 40);
-console.log(`Name: ${teacher1.name}`);
-teacher1.teach(); // This will call the teach method specific to the Teacher class
